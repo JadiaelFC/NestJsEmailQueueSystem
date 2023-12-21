91e1cffc32e4c1d892f2e3c60752c8c5660990be
Jadiael Juvino <jadiael.silva@ferreiracosta.com.br>
2023-12-21 10:55:09 -0300

    feat: Add email consumer event handlers

--- src/mailer/consumers/email.consumer.ts
+++ src/mailer/consumers/email.consumer.ts
@@ -1,5 +1,12 @@
-import { Process, Processor } from '@nestjs/bull';
-import { Job } from 'bull';
+import {
+  OnQueueActive,
+  OnQueueCompleted,
+  OnQueueProgress,
+  OnQueueWaiting,
+  Process,
+  Processor,
+} from '@nestjs/bull';
+import { Job, JobId } from 'bull';
 import { MailerService } from '@nestjs-modules/mailer';
 
 @Processor('email-queue')
@@ -7,6 +14,7 @@ export class EmailConsumer {
   constructor(private readonly mailerService: MailerService) {}
   @Process('send-welcome-email')
   async sendWelcomeEmail(job: Job<any>) {
+    await new Promise((resolve) => setTimeout(resolve, 3000));
     const { email, name } = job.data;
     await this.mailerService.sendMail({
       to: email,
@@ -15,5 +23,25 @@ export class EmailConsumer {
       context: { name: name },
     });
     return 'success';
+  }
+
+  @OnQueueCompleted()
+  onQueueCompleted(job: Job) {
+    console.log(`On Completed ${job.name}`);
+  }
+
+  @OnQueueProgress()
+  onQueueProgress(job: Job) {
+    console.log(`On Progress ${job.name}`);
+  }
+
+  @OnQueueActive()
+  onQueueActive(job: Job) {
+    console.log(`On Active ${job.name}`);
+  }
+
+  @OnQueueWaiting()
+  onQueueWaiting(job: JobId) {
+    console.log(`On Waiting ${job}`);
   }
 }
